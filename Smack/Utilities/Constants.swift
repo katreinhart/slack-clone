3626d22409d48fffff12a5addd196b967701f060
Kat Reinhart <kat.reinhart@gmail.com>
2017-09-24 18:54:38 -0700

    Set up login page layout and add loginVC

---
+++ Smack/Utilities/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  Smack
+//
+//  Created by Katherine Reinhart on 9/24/17.
+//  Copyright © 2017 reinhart.digital. All rights reserved.
+//
+
+import Foundation
+
+// Segues
+
+let TO_LOGIN = "toLogin"
