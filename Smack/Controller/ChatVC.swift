87ce5a84300fb834d85f349ab81cd3c3cce50954
Kat Reinhart <kat.reinhart@gmail.com>
2017-09-24 17:07:12 -0700

    Set up SWReveal and main view controllers

---
+++ Smack/Controller/ChatVC.swift
@@ -0,0 +1,27 @@
+//
+//  ChatVC.swift
+//  Smack
+//
+//  Created by Katherine Reinhart on 9/24/17.
+//  Copyright © 2017 reinhart.digital. All rights reserved.
+//
+
+import UIKit
+
+class ChatVC: UIViewController {
+    
+    // Outlets
+    
+    @IBOutlet weak var menuBtn: UIButton!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        menuBtn.addTarget(self.revealViewController(), action: #selector(SWRevealViewController.revealToggle(_:)), for: .touchUpInside)
+        self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
+        self.view.addGestureRecognizer(self.revealViewController().tapGestureRecognizer())
+    }
+
+    
+
+}
