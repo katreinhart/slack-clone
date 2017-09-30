c228524eee83e3e04bf13a56fde2413a8b59eefe
Kat Reinhart <kat.reinhart@gmail.com>
2017-09-30 11:19:05 -0700

    Implement Message Service and send request to get channels

---
+++ Smack/Model/Channel.swift
@@ -0,0 +1,14 @@
+//
+//  Channel.swift
+//  Smack
+//
+//  Created by Katherine Reinhart on 9/30/17.
+//  Copyright © 2017 reinhart.digital. All rights reserved.
+//
+
+import Foundation
+struct Channel {
+    public private(set) var channelTitle: String!
+    public private(set) var channelDescription: String!
+    public private(set) var id: String!
+}
