0c0e5f4a6b830aa5911d4743da77ea527848c0b0
Kat Reinhart <kat.reinhart@gmail.com>
2017-10-04 19:57:55 -0700

    Build message handler and prepare to send messages (ga .)

---
+++ Smack/Model/Message.swift
@@ -0,0 +1,19 @@
+//
+//  Message.swift
+//  Smack
+//
+//  Created by Katherine Reinhart on 10/4/17.
+//  Copyright © 2017 reinhart.digital. All rights reserved.
+//
+
+import Foundation
+
+struct Message {
+    public private(set) var message : String!
+    public private(set) var userName : String!
+    public private(set) var channelId : String!
+    public private(set) var userAvatar : String!
+    public private(set) var userAvatarColor : String!
+    public private(set) var id : String!
+    public private(set) var timeStamp : String!
+}
