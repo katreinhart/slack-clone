3d97459ce991582e0496d885154934266888083c
Kat Reinhart <kat.reinhart@gmail.com>
2017-10-04 20:40:46 -0700

    Display messages using table view

---
+++ Smack/View/MessageCell.swift
@@ -0,0 +1,33 @@
+//
+//  MessageCell.swift
+//  Smack
+//
+//  Created by Katherine Reinhart on 10/4/17.
+//  Copyright © 2017 reinhart.digital. All rights reserved.
+//
+
+import UIKit
+
+class MessageCell: UITableViewCell {
+    
+    // Outlets
+    @IBOutlet weak var userImg: CircleImage!
+    @IBOutlet weak var userNameLbl: UILabel!
+    @IBOutlet weak var timeStampLbl: UILabel!
+    @IBOutlet weak var messageBodyLbl: UILabel!
+    
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    func configureCell(message: Message) {
+        messageBodyLbl.text = message.message
+        userNameLbl.text = message.userName
+        userImg.image = UIImage(named: message.userAvatar)
+        userImg.backgroundColor = UserDataService.instance.returnUIColor(components: message.userAvatarColor)
+        
+    }
+
+}
